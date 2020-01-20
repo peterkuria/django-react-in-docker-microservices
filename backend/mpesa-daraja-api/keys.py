7858f7cbfd97364aaf23413b43df5afd5306b0e3
peterkuria <peter.kuria.cloud@gmail.com>
2020-01-20 08:29:53 +0300

    updated READme docs

--- backend/mpesa-daraja-api/keys.py
+++
@@ -1,9 +0,0 @@
-
-# firebase-react-starter-kit
-
-# APPNAME=
-business_shortCode = "" # Lipa na mpesa shortcode
-phone_number =  '254706454662' # dont put + infront as +254
-lipa_na_mpesa_passkey = ""
-consumer_key = ""
-consumer_secret = ""
